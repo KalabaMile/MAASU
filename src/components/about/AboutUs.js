8c9638c1264a0eeadc12feebf83a23b44450a33b
KalabaMile <mile.kalaba7@gmail.com>
2023-02-10 17:25:26 +0100

    new-aboutus

--- src/components/about/AboutUs.js
+++ src/components/about/AboutUs.js
@@ -3,7 +3,7 @@ import React from "react";
 const AboutUs = () => {
   return (
     <div className="flex flex-col md:flex-row">
-      <div className="w-full md:w-1/2 text-zinc-400 px-6 border-r-[1px] border-r-zinc-800 border-b-[1px] border-b-zinc-800 flex items-center">
+      <div className="w-full md:w-1/2 text-white px-6 border-r-[1px] border-r-zinc-800 border-b-[1px] border-b-zinc-800 flex items-center">
         <div className="py-5">
           <p className="text-base leading-6 ">
             We are a passionate team building better tomorrow.
@@ -12,22 +12,22 @@ const AboutUs = () => {
           </p>
         </div>
       </div>
-      <div className="w-full md:w-1/2 p-6 border-b-[1px] border-b-zinc-800 ">
-        <ul className="flex flex-col gap-1">
-          <li className="aboutRightLi">
-            <span className="aboutRightLiSpan">Countries Connected:</span>
+      <div className="w-full md:w-1/2 border-b-[1px] border-b-zinc-800 ">
+        <ul className="grid grid-cols-1 md:grid-cols-2 h-full">
+          <li className="py-4 px-4 flex flex-col items-center justify-center gap-2 border border-zinc-800">
+            <span className="">Countries:</span>
             <span className="aboutLeftLiSpan">28</span>
           </li>
-          <li className="aboutRightLi">
-            <span className="aboutRightLiSpan">Partners Worldwide:</span>
+          <li className="lgl:mr-2 py-4 px-4 flex flex-col items-center justify-center gap-2 border border-zinc-800">
+            <span className="">Partners:</span>
             <span className="aboutLeftLiSpan">850+</span>
           </li>
-          <li className="aboutRightLi">
-            <span className="aboutRightLiSpan">Data Sets per Day:</span>
+          <li className="py-4 px-4 flex flex-col items-center justify-center gap-2 border border-zinc-800">
+            <span className="">Data Sets per Day:</span>
             <span className="aboutLeftLiSpan">100M+</span>
           </li>
-          <li className="aboutRightLi">
-            <span className="aboutRightLiSpan">Devices Connected:</span>
+          <li className="lgl:mr-2 py-4 px-4 flex flex-col items-center justify-center gap-2 border border-zinc-800">
+            <span className="">Devices:</span>
             <span className="aboutLeftLiSpan">200K+</span>
           </li>
         </ul>
